84e4aa848ffad5c1034e0b6314a5431ddc632027
JansenQuinten <quiinten@gmail.com>
2017-10-31 15:53:31 +0100

    Opkuis

--- groenetenen/src/main/java/be/vdab/repositories/FiliaalRepository.java
+++ groenetenen/src/main/java/be/vdab/repositories/FiliaalRepository.java
@@ -1,12 +1,10 @@
 package be.vdab.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import be.vdab.entities.Filiaal;
-import be.vdab.valueobjects.PostcodeReeks;
 
 public interface FiliaalRepository extends JpaRepository<Filiaal, Long> {
 	List<Filiaal> findByAdresPostcodeBetweenOrderByNaam(int van, int tot);
--- groenetenen/src/main/java/be/vdab/web/FiliaalController.java
+++ groenetenen/src/main/java/be/vdab/web/FiliaalController.java
@@ -1,7 +1,6 @@
 package be.vdab.web;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
